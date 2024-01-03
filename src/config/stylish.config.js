b6190b87e55dcce5f89666a6c5e57fd2a8e0f2b8
Slobodan Zivanovic <155228488+blitzhetz@users.noreply.github.com>
2024-01-03 10:50:21 +0100

    feat(config): created func for getting user config and merging with default conf

--- src/config/stylish.config.js
+++ src/config/stylish.config.js
@@ -1,4 +1,38 @@
+import path from "node:path";
+import { merge } from "../util/merge.js";
+
 const defaultConfig = {
   content: [],
   theme: {},
 };
+
+// Merge user and default config before build.
+// User need to explicitly set type to module inside package.json
+
+// TODO: Find better way of merging configs so user can use both cjs and mjs type of module.
+async function getUserConfig() {
+  const userConfigRootPath = process.cwd();
+  const userConfigPath = path.resolve(userConfigRootPath, "./stylish.config.js");
+
+  try {
+    const userConfigModule = await import(userConfigPath);
+
+    return userConfigModule.default;
+  } catch {
+    // Testing purpose
+    console.log("No user config file. Using default config.");
+    return {};
+  }
+}
+
+const userConfigPromise = getUserConfig();
+
+const configPromise = userConfigPromise.then((userConfig) => {
+  const config = merge(defaultConfig, userConfig);
+  // Testing purpose
+  console.log(config);
+
+  return config;
+});
+
+export default configPromise;
---
+++ stylish.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: [],
+  theme: {},
+  test: {
+    1: "1",
+    2: "2",
+    testing: {
+      11: [1, 2, 3],
+      22: [1, 2, 3],
+    },
+  },
+};
